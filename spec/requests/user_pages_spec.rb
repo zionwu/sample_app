016a166665bbd2c118cbfbcbd4657d937980bb05
zionwu <zionwu3@gmail.com>
2012-10-09 02:03:08 +0800

    finish

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,7 +54,8 @@ describe "User Pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+        it { should have_link('Sign out')  }    
+     end
     end
   end
 
